64a755abae86087de87ac21169aa1409daab1cf0
Vittoria Erdem <vittoriaerdem@gmail.com>
2019-10-06 02:07:50 -0500

    disable click until all pegs have guesses

--- src/components/ScoreButton/ScoreButton.jsx
+++ src/components/ScoreButton/ScoreButton.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import styles from './ScoreButton.module.css';
 
 const ScoreButton = (props) => (
-<button className={`${styles.button} btn btn-default`}>
-  ✔
+  <button
+    className={`${styles.button} btn btn-default`}
+    disabled={props.disabled}
+    onClick={props.handleScoreClick}
+  >
+    ✔
   </button>
 );
 
