48b1b53602aedf63b90108ba6d00d6246fbcc917
Vittoria Erdem <vittoriaerdem@gmail.com>
2019-10-06 02:08:53 -0500

    add in event handling

--- src/App.js
+++ src/App.js
@@ -10,7 +10,11 @@ const colors = ['#7CCCE5', '#FDE47F', '#E04644', '#B576AD'];
 class App extends Component {
   constructor() {
     super();
-    this.state = {
+    this.state = this.getInitialState();
+  }
+
+  getInitialState() {
+    return {
       selColorIdx: 0,
       guesses: [this.getNewGuess()],
       code: this.genCode()
@@ -37,6 +41,97 @@ class App extends Component {
     return this.state.guesses[lastGuess].score.perfect === 4 ? lastGuess + 1 : 0;
   }
 
+  handleColorSelection(colorIdx) {
+    alert(`color index ${colorIdx} selected!`);
+  }
+
+  handleNewGameClick = () => {
+    this.setState(this.getInitialState());
+  }
+
+  handlePegClick = (pegIdx) => {
+    // Get index of last guess object
+    let currentGuessIdx = this.state.guesses.length - 1;
+
+    // Always replace objects/arrays with NEW ones
+    let guessesCopy = [...this.state.guesses];
+    let guessCopy = {...guessesCopy[currentGuessIdx]};
+    let codeCopy = [...guessCopy.code];
+
+    // Update the NEW code array with the currently selected color
+    codeCopy[pegIdx] = this.state.selColorIdx;
+
+    // Update the NEW guess object
+    guessCopy.code = codeCopy;
+
+    // Update the NEW guesses array
+    guessesCopy[currentGuessIdx] = guessCopy;
+
+    // Update state with the NEW guesses array
+    this.setState({
+        guesses: guessesCopy
+    });
+  }
+
+  handleScoreClick = () => {
+    // Need the index of the current guess object (last object in guesses array)
+    let currentGuessIdx = this.state.guesses.length - 1;
+
+    // Create "working" copies of the "guessed" code and the secret
+    // code so that we can modify them as we compute the number of
+    // perfect and almost without messing up the actual ones in state
+    let guessCodeCopy = [...this.state.guesses[currentGuessIdx].code];
+    let secretCodeCopy = [...this.state.code];
+
+    let perfect = 0, almost = 0;
+
+    // First pass computes number of "perfect"
+    guessCodeCopy.forEach((code, idx) => {
+      if (secretCodeCopy[idx] === code) {
+        perfect++;
+        // Ensure same choice is not matched again
+        // by updating both elements in the "working"
+        // arrays to null
+        guessCodeCopy[idx] = secretCodeCopy[idx] = null;
+      }
+    });
+
+    // Second pass computes number of "almost"
+    guessCodeCopy.forEach((code, idx) => {
+      if (code === null) return;
+      let foundIdx = secretCodeCopy.indexOf(code);
+      if (foundIdx > -1) {
+        almost++;
+        // Again, ensure same choice is not matched again
+        secretCodeCopy[foundIdx] = null;
+      }
+    });
+
+    // State must only be updated with NEW objects/arrays
+        // Always replace objects/arrays with NEW ones
+    let guessesCopy = [...this.state.guesses];
+    let guessCopy = {...guessesCopy[currentGuessIdx]};
+    let scoreCopy = {...guessCopy.score};
+
+    // Set scores
+    scoreCopy.perfect = perfect;
+    scoreCopy.almost = almost;
+
+    // Update the NEW guess with the NEW score object
+    guessCopy.score = scoreCopy;
+
+    // Update the NEW guesses with the NEW guess object
+    guessesCopy[currentGuessIdx] = guessCopy;
+
+    // Add a new guess if not a winner
+    if (perfect !== 4) guessesCopy.push(this.getNewGuess());
+
+    // Finally, update the state with the NEW guesses array
+    this.setState({
+      guesses: guessesCopy
+    });
+  }
+
   render() {
     let winTries = this.getWinTries();
     return (
@@ -51,6 +146,7 @@ class App extends Component {
             <ColorPicker
               colors={colors}
               selColorIdx={this.state.selColorIdx}
+              handleColorSelection={this.handleColorSelection}
             />
             <GameTimer />
             <NewGameButton />
